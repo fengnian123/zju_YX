fbe295ac7bc499993873126c55bd53aa982dc593
Kamikit <57336335+Kamikit@users.noreply.github.com>
2023-07-07 23:17:02 +0800

    Add files via upload

---
+++ mota_mvvm/APP/app.h
@@ -0,0 +1,23 @@
+#ifndef APP_H
+#define APP_H
+
+#include <memory>
+
+class ViewModel;
+class View;
+class Model;
+class Notification;
+
+class App
+{
+private:
+    std::shared_ptr<View> view;
+    std::shared_ptr<Model> model;
+    std::shared_ptr<ViewModel> viewmodel;
+public:
+    App();
+    void run();
+    void Init();
+};
+
+#endif
---
+++ mota_mvvm/main.cpp
@@ -0,0 +1,13 @@
+#include "view/mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //App* app = new App;
+    //app->run();
+    return a.exec();
+}
